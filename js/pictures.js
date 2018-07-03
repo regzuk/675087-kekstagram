1ab4b9cbc7ceec7ce32bdc44361035328e5fe5da
Germann Kuznetsov <germann.kuznetsov@gmail.com>
2018-07-04 00:48:44 +0300

    7-1

--- js/pictures.js
+++ js/pictures.js
@@ -2,6 +2,15 @@
 
 (function () {
 
+  var picturesContainer = document.querySelector('.pictures');
+  var imgFilters = document.querySelector('.img-filters');
+  var filterPopular = imgFilters.querySelector('#filter-popular');
+  var filterNew = imgFilters.querySelector('#filter-new');
+  var filterDiscussed = imgFilters.querySelector('#filter-discussed');
+
+  var picturesList;
+
+
   var createPictureElement = function (pictureOption, pictureTemplate) {
     var pictureElement = pictureTemplate.cloneNode(true);
 
@@ -26,8 +35,61 @@
     return fragment;
   };
 
+  var changeActiveImgFilterButton = function (activeButton) {
+    imgFilters.querySelector('.img-filters__button--active').classList.remove('img-filters__button--active');
+    activeButton.classList.add('img-filters__button--active');
+  };
+
+  var removePictures = function () {
+    var upload = picturesContainer.removeChild(picturesContainer.querySelector('.img-upload'));
+    while (picturesContainer.firstChild) {
+      picturesContainer.removeChild(picturesContainer.firstChild);
+    }
+    picturesContainer.appendChild(upload);
+  };
+
+  var addPictures = function (pictures) {
+    picturesContainer.appendChild(createPictureList(pictures));
+  };
+
+  var updatePictures = function (pictures) {
+    removePictures();
+    addPictures(pictures);
+  };
+
+  var debounceUpdatePictureList = window.utils.debounce(function (pics, button) {
+    updatePictures(pics);
+    changeActiveImgFilterButton(button);
+  });
+
+  var generatePictureOrder = function (arr) {
+    for (var i = arr.length - 1; i >= 0; i--) {
+      var j = Math.floor(i * Math.random());
+      var swap = arr[i];
+      arr[i] = arr[j];
+      arr[j] = swap;
+    }
+    return arr;
+  };
+
+  filterPopular.addEventListener('click', function () {
+    debounceUpdatePictureList(picturesList, filterPopular);
+  });
+
+  filterNew.addEventListener('click', function () {
+    debounceUpdatePictureList(generatePictureOrder(picturesList.slice()).slice(0, 10), filterNew);
+  });
+
+  filterDiscussed.addEventListener('click', function () {
+    debounceUpdatePictureList(picturesList.slice().sort(function (a, b) {
+      return b.comments.length - a.comments.length;
+    }), filterDiscussed);
+  });
+
   window.backend.load(function (pictures) {
-    document.querySelector('.pictures').appendChild(createPictureList(pictures));
+    picturesList = pictures;
+    addPictures(pictures);
+    imgFilters.classList.remove('img-filters--inactive');
   }, window.utils.errorHandler);
 
 })();
